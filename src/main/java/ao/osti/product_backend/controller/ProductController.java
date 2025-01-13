2e25d7b5dc26dfff1368d332049b1e1a25031d14
Socrates Muquepe <socycrates@hotmail.com>
2025-01-13 08:46:10 +0100

    Refactorando o ProductController - adicionando RequestMapping no controller e eliminando as repetições

--- src/main/java/ao/osti/product_backend/controller/ProductController.java
+++ src/main/java/ao/osti/product_backend/controller/ProductController.java
@@ -16,17 +16,19 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @CrossOrigin
+@RequestMapping("products")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @PostMapping("products")
+    @PostMapping
     public ResponseEntity<Product> save(@RequestBody Product product) {
         product = productService.save(product);
         URI location = ServletUriComponentsBuilder
@@ -37,24 +39,24 @@ public class ProductController {
         return ResponseEntity.created(location).body(product);
     }
 
-    @GetMapping("products/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Product> getProduct(@PathVariable int id) {
         Product product = productService.getById(id);
         return ResponseEntity.ok(product);
     }
 
-    @GetMapping("products")
+    @GetMapping
     public List<Product> getProducts() {
         return productService.getAll();
     }
 
-    @DeleteMapping("products/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity<Void> removeProduct(@PathVariable int id) {
         productService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("products/{id}")
+    @PutMapping("{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable int id, @RequestBody Product productUpdate) {
         productService.update(id, productUpdate);
         return ResponseEntity.ok().build();
